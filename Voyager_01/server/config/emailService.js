bc950a2f8065dcd5bb4ddc3792dbd1b029b70466
rajdeep2003 <rajdeep2003002@gmail.com>
2026-05-17 23:27:53 +0530

    add nodemailer password validator

--- Voyager_01/server/config/emailService.js
+++ Voyager_01/server/config/emailService.js
@@ -13,6 +13,19 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER, // Your Gmail address
     pass: process.env.EMAIL_PASSWORD // App password from Gmail
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
+
+  
+transporter.verify((error, success) => {
+  if (error) {
+    console.error('❌ SMTP VERIFY FAILED');
+    console.error(error);
+  } else {
+    console.log('✅ SMTP SERVER IS READY');
   }
 });
 
